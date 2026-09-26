a60d420168203f3048b31945be4b3e855a190f97
Ali <hnourzad.ali@gmail.com>
2026-09-26 19:54:30 +0330

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,18 @@
-window.db = window.supabase.createClient(
-	window.TCHOOBCONFIG.supabaseUrl,
-	window.TCHOOBCONFIG.supabaseAnonKey
-);
+(function () {
+	"use strict";
+
+	if (!window.supabase) {
+		console.error("Supabase CDN has not loaded.");
+		return;
+	}
+
+	if (!window.TCHOOBCONFIG) {
+		console.error("TCHOOBCONFIG is not loaded.");
+		return;
+	}
+
+	window.db = window.supabase.createClient(
+		window.TCHOOBCONFIG.supabaseUrl,
+		window.TCHOOBCONFIG.supabaseAnonKey
+	);
+})();
